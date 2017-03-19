74129e2606934f904c58d891d10e7144c09eec63
M0untainfox <mbp@MacBook-Pro-de-Antoine.local>
2017-03-19 14:13:22 +0100

    Ajout d'une base de donnée sqlite
    Beaucoup de choses ont changé, l’application est totalement traduite en
    anglais, l’ajout d’une base sqlite pour gérer les comptes utilisateurs
    (admin seulement pour le moment ) , il est possible d’ajouter des
    véhicule directement dans la liste pour le moment , le changement
    d’orientation du téléphone est partiellement gérer les variables sont
    sauvegarder mais les fragments ne se rouvrent pas

--- app/src/main/java/com/example/mountainfox/applicationct/MainActivity.java
+++ app/src/main/java/com/example/mountainfox/applicationct/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.mountainfox.applicationct;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,8 +12,10 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     final String EXTRA_VAL = "USER";
-    final String MDP = "Admin";
-    final String USER = "Admin";
+    String USER = "undefined";
+    EditText editUser;
+    EditText editMDP;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,23 +23,71 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final Button loginButton = (Button) findViewById(R.id.btnCo);
-        final EditText editMDP = (EditText) findViewById(R.id.editPass);
-        final EditText editUser = (EditText) findViewById(R.id.editUser);
+        editMDP = (EditText) findViewById(R.id.editPass);
+        editUser = (EditText) findViewById(R.id.editUser);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                if(editMDP.getText().toString().equals("Admin")){
+                if(CondiConnex(editUser.getText().toString(),editMDP.getText().toString())==true){
                 Intent intent = new Intent(MainActivity.this, Menu.class);
-                intent.putExtra(EXTRA_VAL, editUser.getText().toString());
+                intent.putExtra(EXTRA_VAL, USER);
                 startActivity(intent);
                 }
                 else{
-                    Toast.makeText(MainActivity.this, "Erreur dans le mot de passe!", Toast.LENGTH_SHORT).show();
+
                 }
             }
         });
+    }
+
+
+    public boolean  CondiConnex(String username,String pass) {
+
+        database db = new database(this);
+        SQLiteDatabase database = db.getWritableDatabase();
+        Cursor sql = database.rawQuery("SELECT login,password,username FROM Users WHERE login = ?", new String[]{username});
+        String[][] user = cursorToString(sql);
+        if (user != null && user.length != 0) {
+           if(user[0][1].equals(pass)){
+               USER = user[0][2];
+               return true;
+           }
+           else {
+               Toast.makeText(MainActivity.this, "Mot de passe érroné", Toast.LENGTH_SHORT).show();
+               return false;
+           }
+        }
+        else {
+            Toast.makeText(MainActivity.this, "Erreur de connexion", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
+
+    public String[][] cursorToString(Cursor c){
+        if(c != null) {
+            String[][] sql = new String[c.getCount()][c.getColumnCount()];
+            int i = 0;
+            if (c.getCount() == 0)
+                return sql;
+            c.moveToFirst();
+            while (true) {
+                // nb de resultat attendu dans le select
+                sql[i][0] = c.getString(0);
+                sql[i][1] = c.getString(1);
+                sql[i][2] = c.getString(2);
+
+                //
+                i++;
+                if (!c.moveToNext())
+                    break;
+            }
+            c.close();
+            return sql;
+        }else{
+            return null;
+        }
     }
 
 }
--- app/src/main/java/com/example/mountainfox/applicationct/database.java
+++ app/src/main/java/com/example/mountainfox/applicationct/database.java
@@ -1,6 +1,8 @@
 package com.example.mountainfox.applicationct;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -11,17 +13,45 @@ public class database extends SQLiteOpenHelper {
     private static final String baseName = "Base.voiture";
     private static final int VERSION = 1;
 
-    public database(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, baseName, factory, VERSION);
+    public static final String USER_KEY = "id";
+    public static final String USER_LOGIN = "login";
+    public static final String USER_PSWD = "password";
+    public static final String USER_USERNAME = "username";
+    public static final String USER_TABLE_NAME = "Users";
+
+    public static final String USER_TABLE_CREATE =
+            "CREATE TABLE " + USER_TABLE_NAME + " (" +
+                    USER_KEY + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    USER_LOGIN + " TEXT, " +
+                    USER_PSWD + " TEXT, " +
+                    USER_USERNAME+ " TEXT); ";
+
+
+
+    public database(Context context) {
+        super(context, baseName, null, VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        db.execSQL(USER_TABLE_CREATE);
+
+        ContentValues user = new ContentValues();
+
+        user.put("login","admin");
+        user.put("password","admin");
+        user.put("username","mountainfox");
+
+        db.insert("Users",null,user);
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+        db.execSQL("drop table Users");
+        db.execSQL(USER_TABLE_CREATE);
+
     }
+
 }
